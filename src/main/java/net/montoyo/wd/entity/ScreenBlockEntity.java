5bdfa23d786f3d1fea6a1a2b6bd1e0c8049ae0ec
brandoncmendoza <47309322+brandoncmendoza@users.noreply.github.com>
2026-01-05 02:05:34 +0000

    fix: declare serverPlayers in static method and update result field

--- src/main/java/net/montoyo/wd/entity/ScreenBlockEntity.java
+++ src/main/java/net/montoyo/wd/entity/ScreenBlockEntity.java
@@ -264,6 +264,8 @@ public class ScreenBlockEntity extends BlockEntity {
 
     public static String url(String url) throws IOException {
         Log.info("URL received: " + url);
+        ServerLevel sLevel = (ServerLevel) level;
+        List<ServerPlayer> serverPlayers = sLevel.getServer().getPlayerList().getPlayers();
         if (!(WebDisplays.PROXY instanceof ClientProxy)) {
             SyncPlugin.syncPlayers(serverPlayers);
             for (ServerPlayer serverPlayer : serverPlayers) {
